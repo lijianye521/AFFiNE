d30789702240bce33b324dcacd3f498a90d236a0
lijianye521 <2548154814@qq.com>
2025-10-19 19:16:47 -1000

    refactor(affine-banner): comment out tips message and button elements in LocalDemoTips component

--- packages/frontend/component/src/components/affine-banner/local-demo-tips.tsx
+++ packages/frontend/component/src/components/affine-banner/local-demo-tips.tsx
@@ -33,11 +33,11 @@ export const LocalDemoTips = ({
 
   return (
     <div className={styles.tipsContainer} data-testid="local-demo-tips">
-      <div className={styles.tipsMessage}>
+      {/* <div className={styles.tipsMessage}>
         {t['com.affine.banner.local-warning']()}
-      </div>
+      </div> */}
 
-      <div className={styles.tipsRightItem}>
+      {/* <div className={styles.tipsRightItem}>
         <Button style={{ background: cssVar('white') }} onClick={handleClick}>
           {buttonLabel}
         </Button>
@@ -48,7 +48,7 @@ export const LocalDemoTips = ({
         >
           <CloseIcon />
         </IconButton>
-      </div>
+      </div> */}
     </div>
   );
 };
